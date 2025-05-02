fe2ba8840e28d49684e69e6f1792e11058645ba4
hn1f <204560208+hn1fsLandfill@users.noreply.github.com>
2025-05-02 17:16:37 +0200

    Force framework button's theme on MaterialButton

--- app/src/main/java/eu/hn1f/droidcss/Universial.kt
+++ app/src/main/java/eu/hn1f/droidcss/Universial.kt
@@ -10,6 +10,7 @@ import android.graphics.Color
 import android.graphics.PorterDuff.Mode
 import android.util.Log
 import android.view.ContextThemeWrapper
+import android.widget.Button
 import android.widget.CompoundButton
 import android.widget.Switch
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -27,60 +28,25 @@ class Universial {
     }
 
     private fun getAppCompat(themeName: String, resId: Int, res: Resources): Int {
-        return res.getIdentifier(themeName
+        val newThemeName = themeName
             .replace("GoogleMaterial3","Material3")
             .replace("Material3.","AppCompat.")
             .replace("DynamicColors.", "")
             .replace(".TextButton", "")
             .replace(".TonalButton","")
             .replace(".DarkActionBar",".ActionBar")
-            .replace(".MaterialAlertDialog",".AlertDialog"), res.getResourceTypeName(resId), res.getResourcePackageName(resId)
-        )
-    }
+            .replace(".MaterialAlertDialog",".AlertDialog")
 
-    /* private fun modifyMaterial3Views(viewGroup: ViewGroup) {
-        for (i in 0 until viewGroup.childCount) {
-            val childView = viewGroup.getChildAt(i)
-            val resId = childView.context.theme.callMethod("getAppliedStyleResId") as Int
-            val t = childView.resources.getResourceEntryName(resId)
-            //Log.v("DroidCSS","View is using $t as Theme")
-            if(t.contains("MainActivityTheme")) {
-                Log.v("DroidCSS","Forcing AppCompat on Activity Theme")
-                val k = childView.resources.getIdentifier("Theme.AppCompat.DayNight.NoActionBar",
-                    childView.resources.getResourceTypeName(resId),
-                    childView.resources.getResourcePackageName(resId))
-                if(k != 0) {
-                    Log.v("DroidCSS","Success force")
-                    childView.context.setTheme(k)
-                }
-            } else if(t.contains("Material3.")) {
-                Log.v("DroidCSS","Material 3 detected")
-                val isOverlay = "Overlay."
-                var newOverlay = isOverlay
+        var id = res.getIdentifier(newThemeName, res.getResourceTypeName(resId), res.getResourcePackageName(resId))
 
-                if(t.contains(".Button")) {
-                    newOverlay = "."
-                }
-
-                val k = getAppCompat(t.replace(isOverlay, newOverlay), resId, childView.resources)
-                if(k != 0) {
-                    Log.v("DroidCSS","Success!")
-                    childView.context.setTheme(k)
-                } else {
-                    //val k = childView.resources.getIdentifier("Theme.AppCompat.Button", childView.resources.getResourceTypeName(resId), childView.resources.getResourcePackageName(resId))
-                    if(t.contains(".Button")) {
-                        Log.v("DroidCSS", "Success using fallback")
-                        childView.context.setTheme(android.R.style.Widget_Material_Button)
-                    }
-                }
-            }
-            if (childView is ViewGroup) {
-                //dumpChildViewsRecursive(childView, indentationLevel + 1)
-                modifyMaterial3Views(childView)
-            } // else {
-            //}
+        if(id == 0) {
+            id = res.getIdentifier(
+                themeName.replace("ThemeOverlay","Theme"), res.getResourceTypeName(resId), res.getResourcePackageName(resId)
+            )
         }
-    } */
+
+        return id
+    }
 
     @SuppressLint("SetTextI18n")
     fun onLoad(@Suppress("UNUSED_PARAMETER") loadPackageParam: XC_LoadPackage.LoadPackageParam) {
@@ -134,23 +100,6 @@ class Universial {
             }
         }
 
-        /*resTheme.hookConstructor().runBefore { param ->
-            val c: Context = param.args[0] as Context
-            Log.v("DroidCSS","Switchy thingy")
-
-            param.args[0] = ContextThemeWrapper(c,android.R.style.Widget_Material_CompoundButton_Switch)
-            if(param.args[2] != null) {
-                param.args[2] = android.R.style.Widget_Material_CompoundButton_Switch
-            }
-        }
-
-        val res = findClass("android.content.res.Resources")
-        res.hookMethod("getIdentifier").runBefore { param ->
-            val t = param.args[0] as String
-
-            Log.v("DroidCSS","Attempt to obtain $t")
-        } */
-
         val mSwitch = findClass("com.google.android.material.materialswitch.MaterialSwitch")
         //val switchCompat = findClass("androidx.appcompat.widget")
 
@@ -190,11 +139,11 @@ class Universial {
         // implementation of AlertDialog."
         mDialog.hookConstructor().runBefore { param ->
             Log.v("DroidCSS", "Attempt to use MaterialDialog, forcing AppCompat Dialog")
-            var theme = android.R.style.Theme_Material_Light_Dialog
+            var theme = android.R.style.Theme_Material_Light_Dialog_NoActionBar
 
             if(isDarkMode(param.args[0] as Context)) {
                 Log.v("DroidCSS","Using Dark Dialog")
-                theme = android.R.style.Theme_Material_Dialog
+                theme = android.R.style.Theme_Material_Dialog_NoActionBar
             }
 
             param.args[0] = ContextThemeWrapper(param.args[0] as Context, theme)
@@ -209,17 +158,29 @@ class Universial {
         mButton.hookConstructor().runAfter { param ->
             Log.v("DroidCSS", "Attempt to use MaterialButton, forcing framework Button Theme")
             var theme = android.R.style.Widget_Material_Light_Button
-
             if(isDarkMode(param.args[0] as Context)) {
                 Log.v("DroidCSS","Using Dark button")
                 theme = android.R.style.Widget_Material_Button
             }
 
-            (param.thisObject.callMethod("getContext") as Context).setTheme(theme)
+            val db = Button(ContextThemeWrapper(param.args[0] as Context, theme))
+            val b = param.thisObject as Button
 
-            if(param.args.size > 2) {
-                param.args[2] = theme
+            b.background = db.background
+            b.backgroundTintList = db.backgroundTintList
+            b.backgroundTintMode = db.backgroundTintMode
+            b.backgroundTintBlendMode = db.backgroundTintBlendMode
+            if(android.os.Build.VERSION.SDK_INT > 34) {
+                b.highlights = db.highlights
             }
+            b.highlightColor = db.highlightColor
+            b.foreground = db.foreground
+            b.foregroundTintList = db.foregroundTintList
+            b.foregroundTintMode = db.foregroundTintMode
+            b.foregroundTintBlendMode = db.foregroundTintBlendMode
+            b.setTextColor(db.textColors)
+            b.setHintTextColor(db.hintTextColors)
+            b.setLinkTextColor(db.linkTextColors)
         }
 
         Log.v("DroidCSS", "Hooked into app mrrp~~ :3")
