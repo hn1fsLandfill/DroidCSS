0bd31b721f2e4ed7af2191f5ebb45ddb2bd05a33
hn1f <hn1f@proton.me>
2026-02-16 19:28:34 +0100

    add systemuiapplication

---
+++ app/src/main/java/eu/hn1f/droidcss/customSysUI/SystemUIApplication.kt
@@ -0,0 +1,12 @@
+package eu.hn1f.droidcss.customSysUI
+
+import android.app.Application
+
+// todo?
+class SystemUIApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    fun startServices() {}
+}
